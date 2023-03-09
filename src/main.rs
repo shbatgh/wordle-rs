fcba2458de6436c90fa65d630911200bb6ba7191
shbatgh <shboccara@gmail.com>
2023-03-08 23:01:24 -0500

    Reworked evaluate function so that it would work properly.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+#![allow(unused)]
+
 use rand::Rng;
 use std::{
     fs::File,
@@ -5,16 +7,17 @@ use std::{
     path::Path,
     io,
 };
+use colored::*;
 
 #[derive(Copy, Clone, Debug, PartialEq, Eq)]
-pub enum Correctness {
+enum Correctness {
     Correct,
     Incorrect,
     Misplaced,
 }
 
 #[derive(Copy, Clone, Debug, PartialEq, Eq)]
-pub enum Validation {
+enum Validation {
     Valid,
     NotWord,
     Not5Letters,
@@ -22,7 +25,9 @@ pub enum Validation {
 
 fn main() {
     let answers = get_answers("C:/Users/grand/dev/learning_rust/wordle/words/answers.txt");
-    let answer = answers[rand::thread_rng().gen_range(1..=answers.len())].clone();
+    let guesses = get_answers("C:/Users/grand/dev/learning_rust/wordle/words/guesses.txt");
+    //let answer = answers[rand::thread_rng().gen_range(1..=answers.len())].clone();
+    let answer = String::from("polar");
     let mut count = 0;
 
     println!("Welcome to Wordle! You have 5 guesses to guess the word. Good luck!");
@@ -38,6 +43,7 @@ fn main() {
 
         println!("Guess the word: ");
         io::stdin().read_line(&mut guess).expect("Failed to read line");
+        guess.trim();
 
         if guess.trim() == answer.trim() {
             println!("You win!");
@@ -47,7 +53,7 @@ fn main() {
         if guess.trim().len() != 5 {
             println!("Not 5 letters");
             continue;
-        } /*else if !answers.contains(&guess) {
+        } /*else if !guesses.contains(&String::from(guess.trim())) {
             println!("Not a word");
             continue;
         }*/ else {
@@ -55,6 +61,7 @@ fn main() {
         }
 
         evaluate(answer.as_str(), guess.as_str(), count);
+        print_correctness(evaluate(answer.as_str(), guess.as_str(), count), guess.clone());
         println!("{:?}", evaluate(answer.as_str(), guess.as_str(), count));
 
         count += 1;
@@ -63,29 +70,50 @@ fn main() {
 
 fn evaluate(answer: &str, guess: &str, count: i32) -> Vec<Correctness> {
     let mut checker = vec![Correctness::Incorrect; 5];
-    let mut accounted_for: Vec<usize> = Vec::new();
 
-    for (idx, (g, a)) in guess.bytes().zip(answer.bytes()).enumerate() {
+    let mut unaccounted_for = [0; (b'z' - b'a' + 1) as usize];
+    //let mut account_for_repeats: Vec<char> = Vec::new();
 
+    for (idx, (g, a)) in guess.trim().bytes().zip(answer.bytes()).enumerate() {
         if g == a {
             checker[idx] = Correctness::Correct;
-            accounted_for.push(idx as usize);
-        } else if answer.contains(g as char) {
-            let repeats = check_for_repeats(String::from(answer), g as char);
-            for i in repeats {
-                if !accounted_for.contains(&(i as usize)) {
-                    checker[i as usize] = Correctness::Misplaced;
-                    accounted_for.push(i as usize);
-                }
-            }
-            
+        } else {
+            unaccounted_for[(a - b'a') as usize] += 1;
         }
     }
 
-    if count > 0 {
-        checker.clear();
-    }
+    for (idx, g) in guess.trim().bytes().enumerate() {
+        if checker[idx] == Correctness::Correct {
+            continue;
+        }
 
+        if unaccounted_for[(g - b'a') as usize] > 0 {
+            checker[idx] = Correctness::Misplaced;
+            unaccounted_for[(g - b'a') as usize] -= 1;
+        }
+    }
+        
+        /*else if answer.contains(g as char) {
+            /*let repeats = check_for_repeats(String::from(answer.trim()), g as char);
+            let idx_at_answer = &answer.find(g as char).unwrap();
+
+            if !accounted_for.contains(&(idx as usize)) {
+                checker[idx as usize] = Correctness::Misplaced;
+                accounted_for.push(idx as usize);
+                account_for_repeats.push(i as usize);
+            }*/
+            /*for i in repeats {
+                
+            }*/
+            
+            for i in 0..answer.len() {
+                if answer.as_bytes()[i]  == g && !accounted_for.contains(&idx) {
+                    println!("{}", answer.as_bytes()[i]);
+                    checker[i] = Correctness::Misplaced;
+                    accounted_for.push(idx);
+                }
+            }
+        }*/
 
     checker
 }
@@ -99,16 +127,33 @@ fn get_answers(filename: impl AsRef<Path>) -> Vec<String> {
         .collect()
 }
 
-fn check_for_repeats(guess: String, letter: char) -> Vec<i32> {
+fn check_for_repeats(answer: String, letter: char) -> Vec<i32> {
     let mut indices: Vec<i32> = Vec::new();
 
-    for (idx, l) in guess.chars().enumerate() {
+    for (idx, l) in answer.chars().enumerate() {
         if l == letter {
             indices.push(idx as i32);
         }
     }
 
     indices
+}
+
+fn print_correctness(correctness: Vec<Correctness>, guess: String) {
+    for (idx, c) in correctness.iter().enumerate() {
+        match c {
+            Correctness::Correct => print!("{}", (guess.as_bytes()[idx] as char).to_string().green()),
+            Correctness::Incorrect => print!("{}", (guess.as_bytes()[idx] as char).to_string().red()),
+            Correctness::Misplaced => print!("{}", (guess.as_bytes()[idx] as char).to_string().yellow()),
+        }
+    }
+
+    println!();
+}
+
+// Implement the .contains() method for Vec<i32> and return the indice of the letter
+fn contains_return_indices() {
+
 }
 
 #[cfg(test)]
@@ -120,6 +165,11 @@ mod tests {
         assert_eq!(evaluate("hello", "hello", 0), vec![Correctness::Correct, Correctness::Correct, Correctness::Correct, Correctness::Correct, Correctness::Correct]);
         assert_eq!(evaluate("hello", "hella", 0), vec![Correctness::Correct, Correctness::Correct, Correctness::Correct, Correctness::Correct, Correctness::Incorrect]);
         assert_eq!(evaluate("hello", "helle", 0), vec![Correctness::Correct, Correctness::Correct, Correctness::Correct, Correctness::Correct, Correctness::Incorrect]);
+    }
+
+    #[test]
+    fn repeats() {
+        assert_eq!(evaluate("polar", "hello", 0), vec![Correctness::Incorrect, Correctness::Incorrect, Correctness::Correct, Correctness::Incorrect, Correctness::Misplaced]);
     }
 
     #[test]
@@ -145,6 +195,17 @@ mod tests {
     #[test]
     fn same_letter_misplaced_twice() {
         assert_eq!(evaluate("hello", "hleol", 0), vec![Correctness::Correct, Correctness::Misplaced, Correctness::Misplaced, Correctness::Misplaced, Correctness::Misplaced]);
+    }
+
+    #[test]
+    fn papa_bozo() {
+        assert_eq!(evaluate("rrara", "rarrt", 0), vec![Correctness::Correct, Correctness::Misplaced, Correctness::Misplaced, Correctness::Correct, Correctness::Incorrect]);
+    }
+
+    #[test]
+    fn papa_bozo2() {
+        //println!("{:?}", evaluate("rrara", "abase", 0));
+        assert_eq!(evaluate("rrara", "abase", 0), vec![Correctness::Misplaced, Correctness::Incorrect, Correctness::Correct, Correctness::Incorrect, Correctness::Incorrect]);
     }
 
     #[test]
